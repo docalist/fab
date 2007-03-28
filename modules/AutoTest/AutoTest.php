832ad7ece3bbfeb0f8086ea4f88aae9fd44f7acf
Daniel Menard <daniel.menard@laposte.net>
2007-03-28 16:06:03 +0000

    Amélioration conséquente du module d'AutoTest. Affichage plus lisible Permet l'exécution de fichiers "testfile" similaires aux fichiers phpt de php Ne s'arrête plus à la première erreur rencontrée Le bilan affiché en bas de page est maintenant complet (avant on n'avait pas les tests ignorés, incomplets ou en erreur)
    git-svn-id: http://fab.googlecode.com/svn/trunk@284 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- modules/AutoTest/AutoTest.php
+++ modules/AutoTest/AutoTest.php
@@ -16,6 +16,8 @@ class AutoTest extends Module
     const TEST_DIR_PATTERN='tests';
     const TEST_FILE_PATTERN='*Test.php';
     
+    public static $currentSuite=null;
+    
     private function findTests($root, $prefix='/')
     {
         foreach(glob($root.'*', GLOB_ONLYDIR|GLOB_MARK) as $file)
@@ -33,7 +35,7 @@ class AutoTest extends Module
     public function actionIndex()
     {
         $this->findTests(Runtime::$fabRoot);
-        Template::Run('list.xhtml', array('tests'=>$this->tests));
+        Template::Run('list.html', array('tests'=>$this->tests));
     }
     
     public function actionRun()
@@ -43,6 +45,11 @@ class AutoTest extends Module
         	echo "<p>Aucun test n'a été indiqué</p>";
             return;
         }
+
+        $tests=new PHPUnit_Framework_TestSuite('AutoTest des composants sélectionnés');
+        $result = new PHPUnit_Framework_TestResult;
+        $result->addListener(new SimpleTestListener);
+        
         foreach((array) $files as $path)
         {
             $class=substr(basename($path), 0, -4); // /dir/CacheTest.php -> CacheTest
@@ -51,36 +58,110 @@ class AutoTest extends Module
 
             require_once($path);
 
-            $test=new $class();
-            $suite = new PHPUnit_Framework_TestSuite($class);
+            //$test=new $class();
+            $tests->addTest(new AutoTestSuite($class));
+        }    
              
-            $result = new PHPUnit_Framework_TestResult;
-            $result->addListener(new SimpleTestListener);
-             
-            // Run the tests.
-            $suite->run($result);
-            
-            echo '<p>', 
-                 ' Total:' , $result->count(),
-                 ', pass: ', $result->count()-$result->failureCount(),
-                 ', fail: ', $result->failureCount(),
-                 '</p>';
-        }
+        // Run the tests.
+        $tests->run($result);
+        
+        $successCount=$result->count()-$result->errorCount()-$result->failureCount()-$result->notImplementedCount()-$result->skippedCount();
+        
+        $p=round(100 * $successCount / $result->count());
+        $e=round(100 * $result->errorCount() / $result->count());
+        $f=round(100 * $result->failureCount() / $result->count());
+        $n=round(100 * $result->notImplementedCount() / $result->count());
+        $s=round(100 * $result->skippedCount() / $result->count());
 
+        echo '<h1>Bilan des tests</h1>';
+        echo '<ul>';
+        echo '<li class="odd">Total : ', $result->count(), '</li>';
+        echo '<li class="error">errors : ', $result->errorCount(), ' ('.$e.'%)</li>';
+        echo '<li class="fail odd">fail : ', $result->failureCount(), ' ('.$f.'%)</li>';
+        echo '<li class="incomplete">not implemented : ', $result->notImplementedCount(), ' ('.$n.'%)</li>';
+        echo '<li class="skip odd">skip : ', $result->skippedCount(), ' ('.$s.'%)</li>';
+        echo '<li class="pass">pass : ', $successCount, ' ('.$p.'%)</li>';
+        echo '</ul>';
+        
+        echo '<div style="width:100%; border: 1px inset #000; background-color: green;float: left;">';
+        echo '<div style="width:',$e,'%; background-color: red;height: 2em;float: left"></div>';
+        echo '<div style="width:',$f,'%; background-color: darkred;height: 2em;float: left"></div>';
+        echo '<div style="width:',$n,'%; background-color: grey;height: 2em;float: left"></div>';
+        echo '<div style="width:',$s,'%; background-color: blue;height: 2em;float: left"></div>';
+        echo '</div>';
     	
     }
 }
 
+class AutoTestSuite extends PHPUnit_Framework_TestSuite
+{
+    public function __construct($class)
+    {
+        $this->setName($class);
+        $reflex = new ReflectionClass($class);
+
+        foreach ($reflex->getMethods() as $method) 
+        {
+            $name=$method->getName();
+
+            if (substr($name, 0, 8) == 'testfile')
+            {    
+                AutoTest::$currentSuite=$this;
+                $method->invoke(new $class());
+            }
+            elseif (substr($name, 0, 4) == 'test')
+            {    
+                $test=new $class();
+                $test->setName($name);
+                $this->addTest($test);
+            }
+        }
+    }    	
+}
+
 class SimpleTestListener implements PHPUnit_Framework_TestListener
 {
-    private $failed=0;
+    private $success=true;
     private $odd=false;
-    private $incomplete=false;
     
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        static $first=true;
+        
+        if (strpos($suite->getName(), 'AutoTest')===false) echo '<li>';
+        echo '<h1>',$suite->getName(),'</h1>';
+        echo '<ul>';
+        $this->odd=false;
+        $first=false;
+    }
+ 
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        echo '</ul>';
+        if (strpos($suite->getName(), 'AutoTest')!==false) echo '</li>';
+    }
+
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        $this->success=true;
+        $this->incomplete=false;
+        $this->odd=!$this->odd;
+    }
+ 
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+        if ($this->success)
+            echo '<li class="', ($this->odd?'odd ':''), 'pass">', $test->getName(), '</li>';
+    }
+ 
     public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        printf("Error while running test '%s'.\n", $test->getName());
-        throw $e;
+        echo '<li class="', ($this->odd?'odd ':''), 'error">', $test->getName(), ' : ', htmlentities($e->getMessage()),'</li>';
+        $this->success=false;
+//        printf("Error while running test '%s'.\n", $test->getName());
+//        echo '<strong style="color: red">', $e->getMessage(), '</strong>';
+        //throw $e;
+        //$this->failed++;
     }
  
     public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
@@ -90,61 +171,31 @@ class SimpleTestListener implements PHPUnit_Framework_TestListener
         
         if ($e instanceof AssertionNoDiffFailed)
         {
-            echo '<li class="', ($this->odd?'odd ':''), 'fail">', $test->getName(), '(), ligne ', $line, ': ', htmlentities($e->getMessage()),
-            '<div class="diff"><div id="div1">',$e->expected,'</div><div id="div2">',$e->result,'</div></div>',
+            echo '<li class="', ($this->odd?'odd ':''), 'fail">', $test->getName(), ' : ', htmlentities($e->getMessage()),
+            '<div class="diff"><div id="div1">',htmlentities($e->expected),'</div><div id="div2">',htmlentities($e->result),'</div></div>',
             '<script>diff_divs("div1","div2")</script>',
              '</li>';
         }
         else
         {
-            echo '<li class="', ($this->odd?'odd ':''), 'fail">', $test->getName(), '(), ligne ', $line, ': ', htmlentities($e->getMessage()),'</li>';
+            echo '<li class="', ($this->odd?'odd ':''), 'fail">', $test->getName(), ' : ', htmlentities($e->getMessage()),'</li>';
         }
-        $this->failed++;
-        $this->odd=!$this->odd;
+        $this->success=false;
     }
  
     public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-//        printf("Test '%s' is incomplete.\n", $test->getName());
-        $this->incomplete=($e->getMessage() ? $e->getMessage() : 'test incomplet');
+        $reason=($e->getMessage() ? $e->getMessage() : 'test incomplet');
+        echo '<li class="', ($this->odd?'odd ':''), 'incomplete">', $test->getName(), ' : ',$reason, '</li>';
+        $this->success=false;
     }
  
     public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        printf("Test '%s' has been skipped.\n", $test->getName());
+        echo '<li class="', ($this->odd?'odd ':''), 'skip">', $test->getName(), ' : ',$e->getMessage(), '</li>';
+        $this->success=false;
     }
  
-    public function startTest(PHPUnit_Framework_Test $test)
-    {
-        $this->failed=0;
-        $this->incomplete=false;
-        debug && Debug::log('Test : %s()', $test->getName());
-    }
- 
-    public function endTest(PHPUnit_Framework_Test $test, $time)
-    {
-        if ($this->failed==0)
-        {
-            if($this->incomplete)
-                echo '<li class="', ($this->odd?'odd ':''), 'pass incomplete">', $test->getName(), '() ',$this->incomplete, '</li>';
-            else
-                echo '<li class="', ($this->odd?'odd ':''), 'pass">', $test->getName(), '()</li>';
-
-            $this->odd=!$this->odd;
-        }
-    }
- 
-    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
-    {
-        echo '<h1>',$suite->getName(), '</h1>';
-        echo '<ul>';
-        $this->odd=true;
-    }
- 
-    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
-    {
-        echo '</ul>';
-    }
 }
 
 class AutoTestCase extends PHPUnit_Framework_TestCase
@@ -176,6 +227,11 @@ class AutoTestCase extends PHPUnit_Framework_TestCase
         $diff = new Text_Diff($lines1, $lines2);
         return $diff;
     }
+
+//    public function assertThat($value, PHPUnit_Framework_Constraint $constraint, $message = '')
+//    {
+//        parent::assertThat($value, $constraint, $message.'yyy');
+//    }
     
     function assertNoDiff($expected, $result, $message='Le résultat obtenu est différent du résultat attendu')
     {
@@ -190,11 +246,133 @@ class AutoTestCase extends PHPUnit_Framework_TestCase
             $r=$result;
         }
         if ($e !== $r)
-        {
-            
             throw new AssertionNoDiffFailed($expected, $result, $message);
-//        	$this->fail('Le résultat obtenu est différent du résultat attendu');
+    }
+    
+    /**
+     * Exécute chacun des test présents dans le fichier testfile en appellant pour chaque
+     * test le callback indiqué.
+     * 
+     * Un fichier testfile est un moyen pratique d'exécuter plusieurs tests consistant 
+     * à comparer le texte du résultat obtenu avec celui du résultat attendu.
+     * 
+     * Le format des fichiers testfile est inspiré de celui des fichiers phpt de php.
+     * 
+     * Le fichier contient plusieurs tests, séparés par une ligne commencant par "===="
+     * (au moins quatre signes égal).
+     * 
+     * Tout ce qui précède la première séparation est considéré comme du commentaire et
+     * est ignoré. Cela permet de documenter le fichier (historique, numéro de version, etc.)
+     * 
+     * Chaque test se compose de sections. Chaque section commence par une ligne de la forme
+     * '--section--' (c'est-à-dire au moins deux tirets, le nom de la section puis au moins deux tirets)
+     * et se termine au début de la section suivante ou à la fin du test.
+     * 
+     * Des espaces sont autorisés avant et après les deux séries d'au moins deux tirets. 
+     * Le nom de la section n'est pas sensible à la casse des caractères.
+     * 
+     * Dans chaque section, les espaces de début et de fin (espaces, tabulations, retours à la
+     * ligne) sont ignorés.
+     * 
+     * Les sections reconnues sont les suivantes :
+     * 
+     * - --test-- : optionnel, un libellé décrivant le test effectué
+     * 
+     * - --file-- : obligatoire, le source du test à exécuter. Il peut s'agir d'un bout de code
+     * php, d'un template ou de n'importe quoi d'autre qui soit compatible avec le callback
+     * utilisé.
+     * 
+     * - --expect-- : obligatoire : le résultat attendu.
+     * 
+     * - --expect exception ExceptionClassName-- : indique qu'on s'attend à ce que l'exécution du test génère 
+     * une exception. Si ExceptionClassName est indiqué, l'exception générée doit correspondre.
+     * 
+     * - --comment-- : permet de commenter le test, de l'expliquer. Cette section est ignorée par le programme.
+     * 
+     * - --skip-- : permet d'ignorer un test. Le contenu de cette section n'est pas utilisée par le programme,
+     * mais vous pouvez y inclure des commentaires expliquant pourquoi le test est passé.
+     * 
+     * @param string $path le path du fichier testfile à exécuter.
+     * 
+     * @param callback $callback la fonction callback à appeller pour chacun des tests du fichier. 
+     * La fonction callback doit prendre en seul paramètre : le contenu de la section --file-- du fichier
+     * 
+     * @return void
+     */
+    function runTestFile($path, $callback)
+    {
+        // Vérifie le callback
+        if (! is_callable($callback))
+            throw new Exception('Le callback indiqué ne peut pas être appellé');
+            
+        // Charge le fichier de tests
+        $tests=file_get_contents($path);
+        $tests=preg_split('~\s*={4,}\s*~s', $tests);
+        
+        // Ignore tout ce qui précède la première ligne de séparation des tests
+        unset($tests[0]);
+
+        $testSuite=new PHPUnit_Framework_TestSuite('Fichier de test ' . basename($path));
+        
+        // Exécute tous les tests
+//        echo '<li>Exécution du fichier testfile ', $path, '<ul>';
+        foreach ($tests as $test)
+        {
+            // Sépare les différentes sections du test
+            $t=preg_split('~^\s*-{2,}\s*([a-zA-Z ]+)\s*-{2,}\s*~ms', $test, -1, PREG_SPLIT_DELIM_CAPTURE);
+            
+            // Le premier élément doit être vide, sinon, c'est qu'on a du texte entre la ligne de '===' et la première section
+            if (trim($t[0]) !== '')
+                throw new Exception("Erreur dans le fichier de test '$path' : texte '$t[0]' trouvé après la ligne '====='");
+            unset($t[0]);
+            
+            // on a maintenant un tableau dont les indices impairs contiennent les noms de section et les pairs les valeurs
+            $test=array();
+            $name='';
+            foreach ($t as $i=>$section)
+            {
+                // Stocke et vérifie le nom de la section en cours 
+                if ($i % 2 == 1)
+                {
+                    $name=strtolower($section);
+                    if (strtolower(substr($section,0,6))==='expect')
+                    {
+                    	if ('' !== $exception=trim(substr($section,6)))
+                        {
+                            $test['exception']=$exception;
+                        	$name='expect';
+                        }
+                    }
+                    
+                    if(isset($test[$name]))
+                        throw new Exception("Erreur dans le fichier de test '$path', section '$name' répétée pour l'un des test");
+                        
+                    if (! in_array($name,array('test','file','expect','comment','skip')))
+                        throw new Exception("Erreur dans le fichier de test '$path', section '$name' inconnue");
+                } 
+                
+                // Stocke le contenu de la section en cours
+                else
+                {
+                    $test[$name]=$section;
+                }
+            }
+            
+            // Ignore les tests vides ne contenant aucune section (par exemple à la fin du fichier)
+            if (count($test)===0) continue;
+            
+            // Vérifie que les sections obligatoires sont présentes
+            if (!isset($test['skip']))
+                foreach(array('file','expect') as $name)
+                    if (! isset($test[$name]))
+                        throw new Exception("Erreur dans le fichier de test '$path', section '$name' absente dans l'un des tests");
+
+            $testCase=new AutoTestFile($test, $callback);                        
+            $testSuite->addTest($testCase);
+                                        
         }
+        echo '</ul>';
+        AutoTest::$currentSuite->addTest($testSuite);
     }
 }
 
@@ -210,4 +388,78 @@ class AssertionNoDiffFailed extends PHPUnit_Framework_AssertionFailedError
         $this->result=$result;	
     }
 }
+
+class AutoTestFile extends AutoTestCase
+{
+    private $callback=null;
+    private $test=null;
+    
+    public function __construct(Array $test, $callback)
+    {
+    	$this->test=$test;
+        $this->callback=$callback;
+        $this->setName('runit');
+    }
+//    public function ssrun(PHPUnit_Framework_TestResult $result = NULL)
+    public function runit()
+    {
+        $test=$this->test;
+        // Exécute le test
+        if (isset($test['skip']))
+        {
+            $this->markTestSkipped( isset($test['skip']) && $test['skip']!=='' ? $test['skip'] : '--skip-- indiqué');
+        }
+        else
+        {
+//            echo '<li>',isset($test['test']) ? $test['test'] : $test['file'];
+            // on attend un échec
+            if (isset($test['exception']))
+            {
+                try
+                {
+                    $result=call_user_func($this->callback, $test['file']);
+                }
+                catch (Exception $e)
+                {
+                    // Vérifie que c'est bien une exception du bon type
+                    if ((get_class($e) !== $test['exception']) && (! is_subclass_of($e, $test['exception'])))
+                        $this->fail("Le code a généré une exception de type '".get_class($e)."' au lieu d'une exception de type '".$test['exception']."'"); 
+
+                    // Vérifie que les mots attendus figurent dans le message
+                    if (trim($test['expect'])==='') return;
+                    $diff=array_diff
+                    (
+                        preg_split('/\s+/', Utils::convertString($test['expect'])),
+                        preg_split('/\s+/', Utils::convertString($e->getMessage()))
+                    );
+                    if (count($diff))
+                        $this->fail("Le(s) mot(s) '".implode(', ', $diff)."' ne figure(nt) pas dans le message de l'exception obtenue : ".$e->getMessage());
+
+                    // Tout est OK
+                    return;
+                }
+                // Aucune exception n'a été générée
+                $this->fail("Le code aurait dû générer une exception de type ".$test['exception']);	
+            }
+
+            // On attend un succès
+            else
+            {
+                $result=call_user_func($this->callback, $test['file']);
+                if (isset($test['test']))
+                    $this->assertNoDiff($test['expect'],$result, $test['test']);
+                else
+                    $this->assertNoDiff($test['expect'],$result);
+            }
+        }           
+    }
+
+    public function getName()
+    {
+        $test=$this->test;
+        return isset($test['test']) ? $test['test'] : $test['file'];
+    }
+    
+}
+
 ?>
