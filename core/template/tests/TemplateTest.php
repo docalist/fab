bfd1d60e7680ebf7ae1e330b5ed3d79913b23a4b
Youenn Leborgne <Youenn.Leborgne@ehesp.fr>
2007-04-02 15:46:29 +0000

    Ajout de Template.iftag.testfile pour les tests unitaires sur les blocs conditionnels de type if/elseif/else.
    Modification de TemplateTest.php en conséquence.
    
    git-svn-id: http://fab.googlecode.com/svn/trunk@303 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- core/template/tests/TemplateTest.php
+++ core/template/tests/TemplateTest.php
@@ -12,38 +12,7 @@ require_once(dirname(__FILE__).'/../TemplateCode.php');
 
 class TemplateTest extends AutoTestCase
 {
-    
-    // la source de données qu'on passe au template : toujours la même...
-    private $tplDataSrc=array
-    (
-        // formats de variables supportés dans les templates
-        'empty'=>'',
-        'vAccentuée'=>'Valeur 1',
-        'camelCase'=>'Valeur 4',
-        'var_with_underscore'=>'Valeur 5',
-        'vValNoSpaces'=>'Valeur18',
-        'arrayVal'=>array('Elément 1', 'Elément 2', 'Elément 3'),
-        'arrayArray'=>array(array('a', 'b', 'c'), array(0, 1, 2, 3, 4), array('lalalala'), array()),
         
-        // formats de variables considérés comme non valides
-        '_underscore'=>'Valeur 2',
-        '1Digit'=>'Valeur 3',
-        '*star'=>'Valeur 6',
-        '<lesserthan'=>'Valeur 7',
-        '$startDollar'=>'Valeur 8',
-        '^notFound'=>'Valeur 9',
-        '!exclamation'=>'Valeur 10',
-        '?question'=>'Valeur 11',
-        '.dot'=>'Valeur 12',
-        ';semicolon'=>'Valeur 13',
-        ':colon'=>'Valeur 14',
-        '>greatherthan'=>'Valeur 15',
-        'greatherthan>'=>'Valeur 16',
-        'vWith:Colon'=>'Valeur 17'
-        
-    );    
-    
-    
     function setUp()
     {
         Cache::addCache(dirname(__FILE__), dirname(__FILE__) . '/data/cache');
@@ -54,7 +23,7 @@ class TemplateTest extends AutoTestCase
     {
     }
     
-    public function testfileTemplatesMatch()
+    public function nutestfileTemplatesMatch()
     {
         $this->runTestFile(dirname(__FILE__).'/MatchTemplates.testfile',array($this,'templatesMatchCallback'));
     }
@@ -79,7 +48,7 @@ class TemplateTest extends AutoTestCase
         return $result;    	
     }
     
-    public function testfileExpressionParser()
+    public function nutestfileExpressionParser()
     {
         //$this->runTestFile(dirname(__FILE__).'/test.testfile',array($this,'expressionParserCallback')); return;
 
@@ -101,6 +70,47 @@ class TemplateTest extends AutoTestCase
     }
     
     /*
+     * tests pour le compilateur de templates
+     */
+    public function testfileTemplateCompiler()
+    {
+        $this->runTestFile(dirname(__FILE__).'/Template.iftag.testfile', array($this, 'templateCompilerCallback'));
+        
+        
+        
+           
+    }
+    
+    public function templateCompilerCallback($template)
+    {
+        if ($template === '') return '';
+        // la source de données qu'on passe aux templates
+        $data = array
+        (
+            'varFalse'=>false,
+            'varAut'=>'Spécialiste en santé publique', 
+            'varTitorigA'=>'Titre original de niveau \'analytique\'',
+            'varTitorigM'=>'Titre original de niveau "monographique"',
+            'varNull'=>null,
+            'varZero'=>0,
+            'varEmptyString'=>''
+                        
+        );
+
+        $tmp=dirname(__FILE__) . '/tempfile.txt';
+        file_put_contents($tmp, $template);
+        ob_start();
+        Template::run
+        (
+            $tmp,
+            $data
+        );
+        $result=ob_get_clean();
+        unlink($tmp);
+        return $result;
+    }
+    
+    /*
      * A partir du nom du fichier de test, récupère ces données et prépare l'environnement de tests avant
      * d'appeler les fonctions qui vont les exécuter
      * 
@@ -108,54 +118,54 @@ class TemplateTest extends AutoTestCase
      * 
      * @param $testFile string représentant le nom du fichier de tests relativement au répertoire dirname(__FILE__).'/data/'
      */ 
-    function performTest($testFile)
-    {
-        // 'data/template.htm' est réservé pour le fichier temporaire nécessaire à Template::run
-        if ($testFile == 'template.htm' || $testFile == 'template.html')
-            throw new Exception('Aucun fichier de test ne peut s\'appeler "template.htm"'); 
-            
-        if (($tests = file_get_contents($path=dirname(__FILE__) . '/data/' . $testFile)) === false)
-            throw new Exception('Fichier non trouvé : ' . $path);
-        
-        
-        //TODO: fonction qui teste si fichier de test est bien formatté
-        
-        $tests = explode('====', $tests);   // chaque test est séparé du suivant par '===='
-        
-        // TODO : virer
-        echo 'NOMBRE DE TESTS : ' . count($tests) . '<br />';
-        
-        foreach($tests as $test)
-        {
-            // chaque test est au format suivant :
-            // --TEST--Chaîne représentant le nom du test
-            // --FILE--Chaîne représentant contenu du template
-            // --EXPECT--Chaîne représentant le résultat attendu après instanciation du template
-            //
-            // Il faut donc récupérer ses chaînes et en supprimer le tag de début,
-            // à savoir ('--TEST--', '--FILE--' et '--EXPECT--')
-            
-            list($title, $text) = explode('--FILE--', $test);
-            list(,$title) = explode('--TEST--', $title);
-            list($template, $expected) = explode('--EXPECT--', $text);
-            
-            // TODO: enlever ligne ci-dessous
-            echo 'TITLE = ' . $title . '<br />TEMPLATE = ' . $template . '<br />';
-
-
-            // HACK: comme on ne peut pas exécuter directement un template, on passe par un fichier temp
-            $path=dirname(__FILE__) . '/data/template.htm';
-            file_put_contents($path, $template);
-            
-            // Exécute le template et bufferise le résultat
-            ob_start();
-            Template::Run($path, $this->tplDataSrc);
-            $result=ob_get_clean();     // sortie effectuée par le template
-            
-            // Vérifie que le résultat attendu obtenu et le résultat obtenu sont identiques
-            $this->assertNoDiff($expected, $result, $title);
-        }
-    }
+//    function performTest($testFile)
+//    {
+//        // 'data/template.htm' est réservé pour le fichier temporaire nécessaire à Template::run
+//        if ($testFile == 'template.htm' || $testFile == 'template.html')
+//            throw new Exception('Aucun fichier de test ne peut s\'appeler "template.htm"'); 
+//            
+//        if (($tests = file_get_contents($path=dirname(__FILE__) . '/data/' . $testFile)) === false)
+//            throw new Exception('Fichier non trouvé : ' . $path);
+//        
+//        
+//        //TODO: fonction qui teste si fichier de test est bien formatté
+//        
+//        $tests = explode('====', $tests);   // chaque test est séparé du suivant par '===='
+//        
+//        // TODO : virer
+//        echo 'NOMBRE DE TESTS : ' . count($tests) . '<br />';
+//        
+//        foreach($tests as $test)
+//        {
+//            // chaque test est au format suivant :
+//            // --TEST--Chaîne représentant le nom du test
+//            // --FILE--Chaîne représentant contenu du template
+//            // --EXPECT--Chaîne représentant le résultat attendu après instanciation du template
+//            //
+//            // Il faut donc récupérer ses chaînes et en supprimer le tag de début,
+//            // à savoir ('--TEST--', '--FILE--' et '--EXPECT--')
+//            
+//            list($title, $text) = explode('--FILE--', $test);
+//            list(,$title) = explode('--TEST--', $title);
+//            list($template, $expected) = explode('--EXPECT--', $text);
+//            
+//            // TODO: enlever ligne ci-dessous
+//            echo 'TITLE = ' . $title . '<br />TEMPLATE = ' . $template . '<br />';
+//
+//
+//            // HACK: comme on ne peut pas exécuter directement un template, on passe par un fichier temp
+//            $path=dirname(__FILE__) . '/data/template.htm';
+//            file_put_contents($path, $template);
+//            
+//            // Exécute le template et bufferise le résultat
+//            ob_start();
+//            Template::Run($path, $this->tplDataSrc);
+//            $result=ob_get_clean();     // sortie effectuée par le template
+//            
+//            // Vérifie que le résultat attendu obtenu et le résultat obtenu sont identiques
+//            $this->assertNoDiff($expected, $result, $title);
+//        }
+//    }
     
     
     
@@ -168,62 +178,62 @@ class TemplateTest extends AutoTestCase
      * 
      * @param $testFile string représentant le nom du fichier de tests relativement au répertoire dirname(__FILE__).'/data/'
      */ 
-    function performTest_Exceptions($testFile)
-    {
-        // 'data/template.htm' est réservé pour le fichier temporaire nécessaire à Template::run
-        if ($testFile == 'template.htm' || $testFile == 'template.html')
-            throw new Exception('Aucun fichier de test ne peut s\'appeler "template.htm"'); 
-            
-        if (($tests = file_get_contents(dirname(__FILE__) . '\\data\\' . $testFile)) === false)
-            throw new Exception('Fichier non trouvé : ' . dirname(__FILE__) . '\\data\\' . $testFile);
-        
-        //TODO: fonction qui teste si fichier de test est bien formatté
-
-        
-        $tests = explode('====', $tests);   // chaque test est séparé du suivant par '===='
-        
-        // TODO : virer
-        echo 'NOMBRE DE TESTS : ' . count($tests) . '<br />';
-        
-        foreach($tests as $test)
-        {
-            // chaque test est au format suivant :
-            // --TEST--Chaîne représentant le nom du test
-            // --FILE--Chaîne représentant contenu du template
-            //
-            // Il faut donc récupérer ses chaînes et en supprimer le tag de début,
-            // à savoir ('--TEST--' et '--FILE--')
-            
-            list($title, $template) = explode('--FILE--', $test);
-            list(, $title) = explode('--TEST--', $title);
-
-            // TODO: enlever ligne ci-dessous
-            echo 'TITLE = ' . $title . '<br />TEMPLATE = ' . $template . '<br />';
-
-            // HACK: comme on ne peut pas exécuter directement un template, on passe par un fichier temp
-            $path=dirname(__FILE__) . '/data/template.htm';
-            file_put_contents($path, $template);
-            
-            
-            try
-            {
-                // Exécute le template et bufferise le résultat
-                ob_start();
-                Template::Run($path, $this->tplDataSrc);
-                $result=ob_get_clean();     // sortie effectuée par le template
-                
-            }
-            catch (Exception $e)
-            {
-                // TODO: enlever la ligne suivante une fois que Daniel l'aura ajouté à son compilateur de templates
-                
-                ob_end_clean();                   // Ferme la bufferisation ouverte par le compilateur de templates
-                continue;
-            }
-            
-            $this->fail();
-        }
-    }
+//    function performTest_Exceptions($testFile)
+//    {
+//        // 'data/template.htm' est réservé pour le fichier temporaire nécessaire à Template::run
+//        if ($testFile == 'template.htm' || $testFile == 'template.html')
+//            throw new Exception('Aucun fichier de test ne peut s\'appeler "template.htm"'); 
+//            
+//        if (($tests = file_get_contents(dirname(__FILE__) . '\\data\\' . $testFile)) === false)
+//            throw new Exception('Fichier non trouvé : ' . dirname(__FILE__) . '\\data\\' . $testFile);
+//        
+//        //TODO: fonction qui teste si fichier de test est bien formatté
+//
+//        
+//        $tests = explode('====', $tests);   // chaque test est séparé du suivant par '===='
+//        
+//        // TODO : virer
+//        echo 'NOMBRE DE TESTS : ' . count($tests) . '<br />';
+//        
+//        foreach($tests as $test)
+//        {
+//            // chaque test est au format suivant :
+//            // --TEST--Chaîne représentant le nom du test
+//            // --FILE--Chaîne représentant contenu du template
+//            //
+//            // Il faut donc récupérer ses chaînes et en supprimer le tag de début,
+//            // à savoir ('--TEST--' et '--FILE--')
+//            
+//            list($title, $template) = explode('--FILE--', $test);
+//            list(, $title) = explode('--TEST--', $title);
+//
+//            // TODO: enlever ligne ci-dessous
+//            echo 'TITLE = ' . $title . '<br />TEMPLATE = ' . $template . '<br />';
+//
+//            // HACK: comme on ne peut pas exécuter directement un template, on passe par un fichier temp
+//            $path=dirname(__FILE__) . '/data/template.htm';
+//            file_put_contents($path, $template);
+//            
+//            
+//            try
+//            {
+//                // Exécute le template et bufferise le résultat
+//                ob_start();
+//                Template::Run($path, $this->tplDataSrc);
+//                $result=ob_get_clean();     // sortie effectuée par le template
+//                
+//            }
+//            catch (Exception $e)
+//            {
+//                // TODO: enlever la ligne suivante une fois que Daniel l'aura ajouté à son compilateur de templates
+//                
+//                ob_end_clean();                   // Ferme la bufferisation ouverte par le compilateur de templates
+//                continue;
+//            }
+//            
+//            $this->fail();
+//        }
+//    }
     
     
     
@@ -233,90 +243,90 @@ class TemplateTest extends AutoTestCase
     /*
      * Test des zones de données simples qui ne génèrent pas d'exception
      */
-    function nutestDataZone()
-    {
-        $this->performTest('dataZone.htm');
-    }
+//    function testDataZone()
+//    {
+//        $this->performTest('dataZone.htm');
+//    }
     
     
     /*
      * Test des zones de données simples qui doivent générer une exception
      */
-    function nutestDataZone_Exceptions()
-    {
-        $this->performTest_Exceptions('dataZone_Exceptions.htm');
-    }
+//    function nutestDataZone_Exceptions()
+//    {
+//        $this->performTest_Exceptions('dataZone_Exceptions.htm');
+//    }
     
     
     /*
      * Test des blocs et des tags optionnels :
      */
-    function nutestOptional()
-    {
-        $this->performTest('optional.htm');
-    }
+//    function nutestOptional()
+//    {
+//        $this->performTest('optional.htm');
+//    }
     
     
     /*
      * Test des blocs et des tags optionnels devant générer une exception
      */
-     function nutestOptional_Exceptions()
-     {
-         $this->performTest_Exceptions('optional_Exceptions.htm');
-     }
+//     function nutestOptional_Exceptions()
+//     {
+//         $this->performTest_Exceptions('optional_Exceptions.htm');
+//     }
      
      
      /*
       * Test des conditionnelles
       */
-      function nutestConditions()
-      {
-          $this->performTest('conditions.htm');
-      }
+//      function nutestConditions()
+//      {
+//          $this->performTest('conditions.htm');
+//      }
       
       
       /*
        * Test des conditionnelles devant générer des exceptions
        */
-       function nutestConditions_Exceptions()
-       {
-           $this->performTest_Exceptions('conditions_Exceptions.htm');
-       }
+//       function nutestConditions_Exceptions()
+//       {
+//           $this->performTest_Exceptions('conditions_Exceptions.htm');
+//       }
        
        
        /*
         * Test des boucles
         */
-       function nutestLoops()
-       {
-           $this->performTest('loops.htm');
-       }
+//       function nutestLoops()
+//       {
+//           $this->performTest('loops.htm');
+//       }
        
        
        /*
         * Test des boucles devant générer des exceptions
         */
-        function nutestLoops_Exceptions()
-        {
-            $this->performTest_Exceptions('loops_Exceptions.htm');
-        }
+//        function nutestLoops_Exceptions()
+//        {
+//            $this->performTest_Exceptions('loops_Exceptions.htm');
+//        }
         
         
         /*
          * Test des "templates internes"
          */
-         function nutestInternalTpl()
-         {
-             $this->performTest('InternalTpl.htm');
-         }
+//         function nutestInternalTpl()
+//         {
+//             $this->performTest('InternalTpl.htm');
+//         }
          
          
          /*
           * Test des templates internes qui doivent générer des exceptions
           */
-          function nutestInternalTpl_Exceptions()
-          {
-              $this->performTest_Exceptions('InternalTpl_Exceptions.htm');
-          }
+//          function nutestInternalTpl_Exceptions()
+//          {
+//              $this->performTest_Exceptions('InternalTpl_Exceptions.htm');
+//          }
 }
 ?>
