57730bc839f2640e6231fb64c9bd9f76385984b5
Youenn Leborgne <Youenn.Leborgne@ehesp.fr>
2007-04-12 15:25:38 +0000

    Début des tests unitaires sur les générateurs.
    On ne teste pour l'instant que le fonctionnement d'une utilisation des générateurs lorsqu'ils sont
    bien utilisés.
    Les cas de mauvaise utilisation (exception) et d'utilisation complexe restant encore à tester.
    
    git-svn-id: http://fab.googlecode.com/svn/trunk@324 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

---
+++ core/template/autoincludes/generators/tests/GeneratorsTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Classe de tests unitaires des générateurs (contrôles utilisateurs standards basés sur les templates match)
+ * 
+ * S'assurer du bon fonctionnement des templates match pour :
+ * - les cas d'utilisation normale dans un premier temps
+ * - TODO: LES CAS DE "MAUVAISES" UTILISATION ENSUITE (Exceptions, etc.)
+ */
+
+
+require_once(dirname(__FILE__).'/../../../TemplateCompiler.php');
+require_once(dirname(__FILE__).'/../../../TemplateCode.php');
+
+class GeneratorsTest extends AutoTestCase
+{
+    
+//    private $showCompiledSource=false;
+    
+//    // Les données qui sont passées aux templates lors de leur instanciation
+//    private $data = array
+//    (                     
+//    );
+        
+        
+    function setUp()
+    {
+        Cache::addCache(dirname(__FILE__), dirname(__FILE__) . '/cache');
+        Config::set('templates.removehtmlcomments',true);
+    }
+    
+    function tearDown()
+    {
+    }
+    
+    
+    // Fonction de test des template match et son callback
+    public function testfileTemplatesMatch()
+    {
+        $this->runTestFile(dirname(__FILE__).'/Generators.singleLineText.testfile',array($this,'generatorCallback'));
+    }
+
+    public function generatorCallback($template)
+    {
+        if ($template === '') return '';
+
+        $tmp=dirname(__FILE__) . '/tempfile.txt';
+        file_put_contents($tmp, $template);
+        ob_start();
+        
+        Template::run($tmp);
+        
+        $result=ob_get_clean();
+        unlink($tmp);
+        
+        return $result;
+    }
+}
+
+?>
