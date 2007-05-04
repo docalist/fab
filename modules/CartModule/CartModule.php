723a4cc34ed6378f29a1fb9d8706b7fbf6761c8e
Severine Ferron <Severine Ferron@ehesp.fr>
2007-05-04 18:20:52 +0000

    - Ajout d'une variable $hasCategory qui permet de savoir si le panier accepte les catégories ou non. Cela est définit lors du premier ajout d'un item, dans un panier vide. - La fonction preExecute n'exécutait que getCart -> Transfert du code de getCart dans preExecute. - Modification de la structure du panier. Auparavant, un item était associé à une clé et le tableau clé-item pouvait faire partie d'une catégorie. Lors d'un ajout, si un item ayant la même clé figurait déjà dans le panier, il était écrasé. Maintenant, dans chaque catégorie, on a un item auquel est associé sa quantité. On peut donc tout à fait sélectionner plusieurs fois le même item et dans ce cas, la quantité est modifiée --> Modification en conséquence des actions Add, Remove et des fonctions add, remove. - Clear : Auparavant la fonction clear vidait complètement le panier. Maintenant, on a le choix de préciser ou non une catégorie. Si catégorie précisée, seule cette catégorie est supprimée. Action Clear : ajout de la possibilité de préciser un callback et un template.
    git-svn-id: http://fab.googlecode.com/svn/trunk@371 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- modules/CartModule/CartModule.php
+++ modules/CartModule/CartModule.php
@@ -15,36 +15,56 @@ class CartModule extends Module
 {
 	public $cart=array();
     
+    /**
+     * @var boolean Indique si le panier accepte ou non les catégories
+     */
+    public $hasCategory=null;
+    
+    /**
+     * Crée, ou charge s'il existe déjà, le panier indiqué dans la configuration.
+     * Si aucun nom, le panier s'appelle cart.
+     * Le panier sera automatiquement enregistré à la fin de la requête en cours.
+     */
 	public function preExecute()
     {
-		// Crée ou charge le panier
-		$this->getCart();
+        // Récupère le nom du panier
+        $name=Config::get('name', 'cart');
+        
+        // Crée le panier s'il n'existe pas déjà dans la session
+        if (!isset($_SESSION[$name])) 
+        {
+            $_SESSION[$name]=array();
+            $_SESSION[$name.'hascategory']=null;
+        }
+        
+        // Crée une référence entre notre tableau cart et le tableau stocké 
+        // en session pour que le tableau de la session soit automatiquement modifié
+        // et enregistré 
+        $this->cart =& $_SESSION[$name];        
+        $this->hasCategory=& $_SESSION[$name.'hascategory'];
 	}
      
+
 	/**
-	 * actionAdd : ajoute un item dans le panier, associé à la clé key
-	 * Si une catégorie est précisée, l'item sera ajouté à cette catégorie
-	 * avec la clé key.
+	 * Ajoute un item dans le panier, en précisant sa quantité.
+	 * Si une catégorie est précisée, l'item sera ajouté à cette catégorie.
 	 */
 	public function actionAdd()
 	{		
 		// Récupère la catégorie
 		$category=Utils::get($_REQUEST['category']);
 		
-		// Récupère la clé
-		$key=Utils::get($_REQUEST['key']);
-		
 		// Récupère l'item à ajouter
 		$item=Utils::get($_REQUEST['item']);
 		
-		// Ajoute l'item au panier
-		$this->add($item, $key, $category);
+		// Récupère la quantité
+		$quantity=Utils::get($_REQUEST['quantity'],1);
 		
-		echo'<pre>';
-		print_r($this->cart);
-		echo'</pre>';
+		// Ajoute l'item au panier
+		$this->add($item, $quantity, $category);
 		
 		// Détermine le callback à utiliser
+		// TODO : Vérifier que le callback existe
 		$callback=Config::get('callback');
 		
 		// Exécute le template, s'il a été indiqué
@@ -53,13 +73,12 @@ class CartModule extends Module
 			(
 				$template,
 				array($this, $callback),
-				$this->cart,
-				array('cart', $this->cart)
+                array('category'=>$category, 'item'=>$item, 'quantity'=>$quantity)
 			);
 	}
 	
 	/**
-	 * actionRemove : supprime un item du panier. Si une catégorie a été précisée,
+	 * Supprime un item du panier. Si une catégorie a été précisée,
 	 * supprime, de cette catégorie, l'item associé à la clé key. Sinon, supprime
 	 * l'item associé à la clé key.
 	 */
@@ -68,19 +87,17 @@ class CartModule extends Module
 		// Récupère la catégorie
 		$category=Utils::get($_REQUEST['category']);
 		
-		// Récupère la clé
-		$key=Utils::get($_REQUEST['key']);
-		
-		// Récupère l'item à ajouter
+		// Récupère l'item à supprimer
 		$item=Utils::get($_REQUEST['item']);
 		
-		// Si item non présent
+		// Récupère la quantité
+		$quantity=Utils::get($_REQUEST['quantity'],1);
 		
 		// Supprime l'item du panier
-		// TODO : Si plus rien dans la catégorie, supprimer la catégorie ?
-		$this->remove($key, $category);
+		$this->remove($item, $quantity, $category);
 		
 		// Détermine le callback à utiliser
+		// TODO : Vérifier que le callback existe
 		$callback=Config::get('callback');
 		
 		// Exécute le template, s'il a été indiqué
@@ -89,24 +106,50 @@ class CartModule extends Module
 			(
 				$template,
 				array($this, $callback),
-				$this->cart,
-				array('cart', $this->cart)
+                array('category'=>$category, 'item'=>$item, 'quantity'=>$quantity)
 			);
 	}
 	
 	/**
-	 * actionClear : vide le panier
+	 * Vide le panier ou une catégorie du panier
 	 */
 	public function actionClear()
 	{
-		$this->clear();		
+        // Récupère la catégorie
+        $category=Utils::get($_REQUEST['category']);
+
+		// Vide la catégorie
+		$this->clear($category);	
+        
+		// Détermine le callback à utiliser
+		// TODO : Vérifier que le callback existe
+		$callback=Config::get('callback');
+
+		// Exécute le template, s'il a été indiqué
+		if ($template=Config::get('template'))
+			Template::run
+			(
+				$template,
+				array($this, $callback),
+                array('category'=>$category)
+			);
 	}
 	
 	/**
-	 * actionShow : affiche le panier
+	 * Affiche le panier
 	 */
 	public function actionShow()
 	{
+		// Vérifie que la catégorie existe
+        if ($category=Utils::get($_REQUEST['category']))
+        {
+        	if ($this->hasCategory)
+        	{
+	        	if (! isset($this->cart[$category]))
+	        		throw new Exception('La catégorie demandée n\'existe pas.');
+        	}
+        }
+        
 		// Détermine le template à utiliser
 		if (! $template=Config::get('template'))
 			throw new Exception('Le template à utiliser n\'a pas été indiqué');
@@ -118,47 +161,59 @@ class CartModule extends Module
 		Template::run
 		(
 			$template,
-			array('cart', $this->cart),
 			array($this, $callback),
-			$this->cart
+            array('category'=>$category)
 		);
 	}
 	
-	/**
-	 * Crée, ou charge s'il existe déjà, le panier indiqué dans la configuration
-     * Le panier sera automatiquement enregistré à la fin de la requête en cours.
-	 */
-	private function getCart()
-	{
-		// Récupère le nom du panier
-		$name=Config::get('name', 'cart');
-		
-        // Crée le panier s'il n'existe pas déjà dans la session
-        if (!isset($_SESSION[$name])) 
-            $_SESSION[$name]=array();
-        
-        // Crée une référence entre notre tableau cart et le tableau stocké 
-        // en session pour que le tableau de la session soit automatiquement modifié
-        // et enregistré 
-        $this->cart =& $_SESSION[$name];		
-	}
-	
+    
     /**
      * Ajoute un item dans le panier. Si un item ayant la même clé figurait déjà
      * dans le panier, il est écrasé.
      * 
-     * @param mixed $item les données à associer à la clé $key
-     * @param mixed $key la clé de l'item à ajouter
+     * @param mixed $item l'élément à ajouter
+     * @param mixed $quantity la quantité d'élément $item
      * @param mixed $category optionnel la catégorie dans laquelle on veut ajouter
-     * l'item 
+     * l'item
      */
-    private function add($item, $key, $category=null)
+    private function add($item, $quantity=1, $category=null)
     {
-		if (is_int($key) || ctype_digit($key)) $key=(int)$key;
-		
+        if ($quantity<0) return $this->remove($item, $quantity, $category);
+        
+        // Le 1er ajout d'un item définit si le panier a des catégories ou pas
+        if (is_null($this->hasCategory))
+            $this->hasCategory=(!is_null($category));
+        else
+        {
+            if ($this->hasCategory)
+            {
+                if (is_null($category)) throw new Exception('Vous devez spécifier une catégorie');
+            }
+            else
+            {
+                if (!is_null($category)) throw new Exception('Catégorie non autorisée');
+            }
+        }
+        
+		if (is_int($item) || ctype_digit($item)) $item=(int)$item;     	
+
 		// Ajoute l'item dans le panier
-		(! is_null($category)) ? $this->cart[$category][$key]=$item : $this->cart[$key]=$item;			
-    } 
+		if (is_null($category))
+        {
+            if (isset($this->cart[$item])) 
+                $this->cart[$item]+=$quantity; 
+            else      
+                $this->cart[$item]=$quantity; 
+        }
+        else
+        {        
+            if (! isset($this->cart[$category]) || ! isset($this->cart[$category][$item]))
+                $this->cart[$category][$item]=$quantity;
+            else
+                $this->cart[$category][$item]+=$quantity;
+        } 
+    }
+    
      
     /**
      * Supprime un élément du panier 
@@ -173,30 +228,73 @@ class CartModule extends Module
      * @param mixed $category optionnel la catégorie dans laquelle on veut supprimer
      * l'item 
      */
-    public function remove($key, $category=null)
+    private function remove($item, $quantity=1, $category=null)
     {
-        if (is_int($key) || ctype_digit($key)) $key=(int)$key;
-        
+        if (is_int($item) || ctype_digit($item)) $item=(int)$item;
+
+        if (! is_null($this->hasCategory))
+        {
+            if ($this->hasCategory)
+            {
+                if (is_null($category)) throw new Exception('Vous devez spécifier une catégorie');
+            }
+            else
+            {
+                if (!is_null($category)) throw new Exception('Catégorie non autorisée');
+            }
+        }
+                
         // Supprime l'élément du panier
         if (is_null($category))
-        	unset($this->cart[$key]);
+        {
+        	if (! isset($this->cart[$item])) return;
+            $this->cart[$item]-=$quantity;
+            if ($this->cart[$item]<=0) unset($this->cart[$item]);
+        }
         else
         {
-        	unset($this->cart[$category][$key]);
+            if (! isset($this->cart[$category])) return;
+            if (! isset($this->cart[$category][$item])) return;
 
+            $this->cart[$category][$item]-=$quantity;
+            if ($this->cart[$category][$item]<=0) unset($this->cart[$category][$item]);
+            
 	        // Si plus d'élément dans la catégorie, supprime la catégorie
 			if (count($this->cart[$category]) == 0)
 				unset($this->cart[$category]);        	
         }
+
+        // Si le panier est vide, réinitialise le flag hasCategory
+        if (count($this->cart) == 0) $this->hasCategory=null;
     }
+    
 
     /**
-     * Vide le panier
+     * Vide la totalité du panier ou supprime une catégorie d'items du panier 
+     * 
+     * @param string|null $category la catégorie à supprimer
      */
-    private function clear()
+    private function clear($category=null)
     {
-    	// Vide le panier
-    	$this->cart=null;    			
+        if ($this->hasCategory)
+        {
+            if (is_null($category)) throw new Exception('Vous devez spécifier une catégorie');
+        }
+        else
+        {
+            if (!is_null($category)) throw new Exception('Catégorie non autorisée');
+        }
+
+        // Si on n'a aucune catégorie, vide tout le panier
+        if (is_null($category))
+        	$this->cart=array();
+            
+        // Sinon, vide uniquement la catégorie indiquée
+        else
+        	unset($this->cart[$category]);
+
+        // Si le panier est complètement vide, réinitialise le flag hasCategory
+        if (count($this->cart) == 0) $this->hasCategory=null;
     }
 	
 } 
