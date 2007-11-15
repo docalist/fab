2a59a49ec1bb2cb42c07df0dfe5d8b8c63a14c59
Daniel Menard <daniel.menard@laposte.net>
2007-11-15 18:33:23 +0000

    Utils::convertString() : modification de la table de conversion des caractères utilisées par le queryParser
    git-svn-id: http://fab.googlecode.com/svn/trunk@516 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- core/utils/Utils.php
+++ core/utils/Utils.php
@@ -395,15 +395,15 @@ final class Utils
                     /* D0 */    'dnooooo 0uuuuy s'.
                     /* E0 */    'aaaaaaaceeeeiiii'.
                     /* F0 */    'dnooooo  uuuuyby',
-
+     
                 'queryparser'=>
                     /*           0123456789ABCDEF*/
                     /* 00 */    '                '.
                     /* 10 */    '     §          '.
-                    /* 20 */    '  "     ()*+ -  '.
-                    /* 30 */    '0123456789:  =  '.
+                    /* 20 */    '  "     ()*+ -. '.
+                    /* 30 */    '0123456789:  :  '.
                     /* 40 */    '@abcdefghijklmno'.
-                    /* 50 */    'pqrstuvwxyz[ ]  '.
+                    /* 50 */    'pqrstuvwxyz[ ] _'.
                     /* 60 */    ' abcdefghijklmno'.
                     /* 70 */    'pqrstuvwxyz     '.
                     /* 80 */    '                '.
