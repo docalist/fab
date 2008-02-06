61abcf201e9e1b44a8580d183dbae6150ec19259
Daniel Menard <daniel.menard@laposte.net>
2008-02-06 17:26:36 +0000

    Nouveau module ThesaurusModule permettant la consultation d'un thesaurus monolingue/monohiérarchique : - module ThesaurusModule - classe utilitaire ThesaurusCindoc utilisée pour décrypter les magnifiques fichiers d'export que génère cindoc - templates - fichier de config - feuille de style css
    Remarque : des essais d'affichage graphique et d'arborescence ont été commencés.
    Pour ne pas perdre le travail fait, j'ai committé ces essais :
    - template nav.html et show-graphique-NU.html
    - feuille de style thesaurus-graphique-NU.css
    - toutes les icones 
    - le code javascript présent dans treeview.js
    
    git-svn-id: http://fab.googlecode.com/svn/trunk@606 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

---
+++ modules/ThesaurusModule/ThesaurusModule.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Module de consultation de thesaurus (monolingue, monohiérarchique)
+ * 
+ * @package     fab
+ * @subpackage  modules
+ */
+class ThesaurusModule extends DatabaseModule
+{
+    public function preExecute()
+    {
+//        if (Utils::isAjax())
+//            $this->setLayout('none');	
+    }
+    
+    public function actionImportAsco()
+    {
+        require_once dirname(__FILE__).'/ThesaurusCindoc.php';
+        
+        // Path du fichier thesaurus à importer
+        $alphaPath=dirname(__FILE__) . '/alpha-20-06-07.txt';
+        $hierPath=dirname(__FILE__) . '/hier-20-06-07.txt';
+        
+        // Charge le fichier thesaurus
+        $theso=new ThesaurusCindoc($alphaPath, $hierPath);
+
+        // Ouvre la base
+        $this->openDatabase(false);
+        
+        // Charge tous les termes dans la base
+        $nb=0;
+        foreach($theso->getTerms() as $fre=>$term)
+        {
+            // Crée une nouvelle notice
+            $this->selection->addRecord();
+            
+            // Initialise tous les champs
+            foreach($term as $rel=>$value)
+                $this->selection[$rel]=$value;
+            
+            // Ajoute la notice dans la base
+            $this->selection->saveRecord();
+
+            // 
+            $nb++;
+            echo $nb, ' ', $fre, '<br />';
+        }
+        
+        // Ferme le fichier thesaurus
+        unset($theso);
+
+        // Ferme la base
+        unset($this->selection);
+    }
+}
+?>
