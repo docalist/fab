398985b721a5eeb33a18ec5a0dc30fa14b350738
Daniel Menard <daniel.menard@laposte.net>
2008-07-16 15:01:39 +0000

    AdminDatabase : - actionBackup() et template associé. - surcharge spécifique pour les breadcrumbs. - code préliminaire pour l'actionRestore(). - index.html : ajout d'un bouron "sauvegarde" pour chaque base.
    git-svn-id: http://fab.googlecode.com/svn/trunk@866 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- modules/AdminDatabases/AdminDatabases.php
+++ modules/AdminDatabases/AdminDatabases.php
@@ -98,7 +98,29 @@ class AdminDatabases extends Admin
         
         return $info;
     }
+    
+    /**
+     * Retourne un tableau permettant de construire un fil d'ariane 
+     * (breadcrumbs).
+     * 
+     * {@inheritdoc}
+     * 
+     * La méthode ajoute au tableau retourné par la classe parente le nom du
+     * fichier éventuel passé en paramètre dans <code>$file</code>.
+     * 
+     * @return array
+     */
+    protected function getBreadCrumbsArray()
+    {
+        $breadCrumbs=parent::getBreadCrumbsArray();
 
+        // Si on a un nom de base en paramêtre, on l'ajoute
+        if ($file=$this->request->get('database'))
+            $breadCrumbs[$this->request->getUrl()]=$file;
+        
+        return $breadCrumbs;    
+    }
+    
     
     /**
      * Page d'accueil du module d'administration des bases de données.
@@ -453,6 +475,470 @@ class AdminDatabases extends Admin
         
         // Redirige vers la page d'accueil
         Runtime::redirect('/'.$this->module);
+    }
+    
+    private function xmlEntities($data)
+    {
+        /*
+         * XML 1.0 définit la liste des caractères unicode autorisés dans 
+         * un fichier xml de la façon suivante :
+         * 
+         * #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
+         * (source : http://www.w3.org/TR/REC-xml/#charsets)
+         * 
+         */
+        
+//        $result=preg_replace_callback
+//        (
+//            '~[^\x09\x0A\x0D\x20-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]~u',
+//            array($this, 'xmlEntitiesCallback'),
+//            $data
+//        );
+
+        /*
+         * Dans notre cas, la chaine passée est de l'ansi. On ne teste que les
+         * caractères < 255
+         */
+        $result=preg_replace_callback
+        (
+            '~[^\x09\x0A\x0D\x20-\xFF]~u',
+            array($this, 'xmlEntitiesCallback'),
+            $data
+        );
+        echo var_export($result, true), '<br />';
+        return $result;
+    }
+    
+    private function xmlEntitiesCallback($match)
+    {
+        echo 'caractère illégal : ' , var_export($match[0], true), ', code=', ord($match[0]), '<br />';
+        return '&#'.ord($match[0]).';';
+        
+    }
+    
+    private function xmlWriteField(XmlWriter $xml, $tag, $value)
+    {
+        if (is_null($value)) return;
+        
+        if (is_array($value))
+        {
+            $xml->startElement($tag);
+            foreach($value as $item)
+            {
+                $this->xmlWriteField($xml, 'item', $item);
+            }
+            $xml->endElement($tag);
+            return;
+        }
+        
+        if (is_bool($value))
+        {
+            $xml->writeElement(utf8_encode($tag), $value ? 'TRUE' : 'FALSE');
+            return;                
+        }
+        
+        if (is_int($value) || is_float($value))
+        {
+            $xml->writeElement(utf8_encode($tag), (string) $value);
+            return;
+        }
+        
+        if (is_string($value))
+        {
+            /*
+             * XML 1.0 définit la liste des caractères unicode autorisés dans 
+             * un fichier xml de la façon suivante :
+             * 
+             * #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
+             * (source : http://www.w3.org/TR/REC-xml/#charsets)
+             * 
+             * Tout autre caractère fera que le fichier xml sera mal formé et ne
+             * pourra pas être chargé.
+             * 
+             * Dans fab, un champ, au moins en théorie, peut contenir des 
+             * caractères binaires. On a le cas dans ascoweb avec la notice 
+             * 90691 dont le résumé contient "\x05\x05" (?).
+             * 
+             * Il faut donc qu'on vérifie tous les caractères. Dans notre cas, 
+             * la chaine passée est de l'ansi. On ne teste donc que les 
+             * caractères [0-255].
+             * 
+             * Si la chaine passée est "propre", on l'écrit telle quelle. Sinon,
+             * on va encoder la totalité de la chaine en base64 et on va ajouter
+             * l'attribut base64="true" au tag.
+             * 
+             * Remarque : dans notre traitement on suppose implicitement que
+             * si une chaine ansi ne contient pas de caractères illégaux, la
+             * chaine obtenue après appel de utf8_encode() n'en contiendra pas
+             * non plus.
+             * 
+             */
+            if (0===preg_match('~[^\x09\x0A\x0D\x20-\xFF]~u', $value))
+            {
+                $xml->writeElement(utf8_encode($tag), utf8_encode($value));
+            }
+            else
+            {
+                $xml->startElement($tag);
+
+//                $xml->writeAttribute('xsi:type', 'xsd:hexBinary');
+//                $xml->text(bin2hex($value));
+                
+                $xml->writeAttribute('xsi:type', 'xsd:base64Binary');
+                $xml->text(base64_encode($value));
+
+                $xml->endElement($tag);
+            }
+            return;
+        }
+        
+        throw new Exception('Type de valeur non géré : ', debug_zval_dump($value));
+    }
+    
+     /**
+     * Backup
+     * 
+     * Fonction récupérée du répertoire 
+     * WebApache\Fab septembre 2007, avant checked out from google\modules\DatabaseAdmin
+     */ 
+    public function actionBackup($database, $taskTime='', $taskRepeat='')
+    {
+        // Détermine un titre pour la tâche de dump
+        $title=sprintf
+        (
+            'Dump %s de la base %s', 
+            ($taskRepeat ? 'périodique' : 'ponctuel'), 
+            $database
+        );
+        
+        // Vérifie que le répertoire /data/backup existe, essaie de le créer sinon
+        $dir=Runtime::$root.'data' . DIRECTORY_SEPARATOR . 'backup' . DIRECTORY_SEPARATOR;
+        if (!is_dir($dir))
+        {
+            if (! @mkdir($dir))
+                throw new Exception('Impossible de créer le répertoire ' . $dir);
+        }
+        
+        // Partie interface utilisateur
+        if (! User::hasAccess('cli'))
+        {
+            // 1. Permet à l'utilisateur de planifier le dump 
+            if ($taskTime==='')
+            {
+                Template::Run('backup.html', array('error'=>''));
+                return;
+            }
+            
+            // 2. Crée la tâche
+            $id=Task::create()
+                ->setRequest($this->request->keepOnly('database'))
+                ->setTime($taskTime)
+                ->setRepeat($taskRepeat)
+                ->setLabel($title)
+                ->setStatus(Task::Waiting)
+                ->save()
+                ->getId();
+    
+            if ($taskTime===0 || abs(time()-$taskTime)<30)
+                Runtime::redirect('/TaskManager/TaskStatus?id='.$id);
+            else
+                Runtime::redirect('/TaskManager/Index');
+            return;
+        }
+                
+        // 3. Lance le dump
+        echo '<h1>', $title, '</h1>';
+        
+        echo '<p>Date du dump : ', strftime('%x %X'), '</p>';
+                        
+        $selection=Database::open($database, true);
+        $selection->search('', array('_sort'=>'+', "_max"=>-1));
+        $count=$selection->count();
+        echo '<p>La base contient ', $count, ' notices</p>';
+        
+        // Détermine le path du fichier à générer
+        $path=$dir . $database . '-' . strftime('%Y%m%d-%H%M%S') . '.xml.gz';
+        
+        echo '<p>Génération du fichier ', $path, '</p>';
+        if (file_exists($path))
+        {
+            throw new Exception('Le fichier de dump existe déjà.');
+        }
+
+        $gzPath='compress.zlib://'.$path;
+        // à tester : stockage sur un ftp distant ?
+        
+        // Ouvre le fichier
+        $xml=new XmlWriter();
+        $xml->openUri($gzPath);
+        //$xml->setIndent(true);
+        $xml->setIndentString('    ');
+        
+        // Génère le prologue xml
+        $xml->startDocument('1.0', 'UTF-8', 'yes');
+        
+        // Tag racine
+        $xml->startElement('database');     // <database>
+        
+        // Ajoute une référence de schema.  Permet d'utiliser xsi:nil pour les
+        // valeurs null et xsi:type pour indiquer le type d'un champ
+        $xml->writeAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
+        $xml->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        
+        // Autres attributs du tag racine
+        $xml->writeAttribute('name', $database);
+        $xml->writeAttribute('timestamp', time());
+        $xml->writeAttribute('date', strftime('%x %X'));
+               
+        // Schéma de la base
+        $xml->writeRaw("\n");
+        $xml->writeRaw($selection->getSchema()->toXml(false, '    '));
+        
+        // Données
+        $xml->startElement('records');     // <records>
+        foreach($selection as $i=>$record)
+        {
+            $xml->startElement('row');      // <row>
+            foreach($record as $field=>$value)
+            {
+                $this->xmlWriteField($xml, $field, $value);
+            }
+            $xml->endElement();             // </row>
+            TaskManager::progress($i, $count);
+        }
+        $xml->endElement();                 // </records>
+        
+        // spellings, synonyms, meta data...
+        
+        // Finalise le fichier xml
+        $xml->endElement();                 // </database>
+        $xml->flush();
+        $xml=null;
+        TaskManager::progress();
+        
+        echo '<p>Dump terminé.</p>';
+        echo '<p>Taille du fichier : ', Utils::formatSize(filesize($path)), '</p>';
+        
+        // Suppression des dump les plus anciens
+        $maxDumps=$this->ConfigDatabaseGet('maxdumps', $database, 7);  // Nombre maximum de dumps par base de données (default : 7 jours)
+        $maxDays=$this->ConfigDatabaseGet('maxdays', $database, 0);    // Nombre de jours pendant lesquels les dumps sont conservés (default : tous)
+
+        echo '<h2>Suppression éventuelle des dumps antérieurs</h2>';
+        echo '<p>Paramètres : </p>';
+        echo '<ul>';
+        echo '<li>Nombre maximum de dumps autorisés pour la base ', $database, ' : ', ($maxDumps===0 ? 'illimité' : $maxDumps), '</li>';
+        echo '<li>Durée de conservation des dumps de la base ', $database, ' : ', ($maxDays===0 ? 'illimitée' : $maxDays.' jour(s)'), '</li>';
+        echo '</ul>';
+        
+        // Calcule la date "maintenant moins maxDays, à minuit"
+        $t=getDate(($maxDays===0) ? 0 : time());
+        $t=mktime(0,0,0,$t['mon'],$t['mday']-$maxDays+1,$t['year']);
+        
+        // Nom minimum d'un fichier
+        $minName=$database . '-' . strftime('%Y%m%d-%H%M%S', $t);
+        
+        if ($maxDumps===0) $maxDumps=PHP_INT_MAX;
+        
+        $files=glob($dir . $database . '-????????-??????.xml.gz');
+        sort($files);
+        
+        echo '<p>Fichiers supprimés : </p>';
+        $nb=0;
+        echo '<ul>';
+        foreach($files as $index=>$path)
+        {
+            $file=basename($path);
+            if (($file<$minName) || ($index<(count($files)-$maxDumps)))
+            {
+                echo '<li>Suppression de ', $file;
+                if (! @unlink($path)) echo ' : erreur'; else ++$nb;
+                echo '</li>';
+            }
+        }
+        if ($nb===0) echo '<li>aucun.</li>';
+        echo '</ul>';
+        
+    }
+    /**
+     * Fonction utilitaire utilisée par actionBackup pour
+     * récupérer la valeur de maxDumps et maxDays dans la config.
+     *
+     * @param string $key
+     * @param string $database
+     * @param int $default
+     */
+    private function ConfigDatabaseGet($key, $database, $default)
+    {
+        $t=Config::get($key, $default);
+        if (is_array($t))
+        {
+            $t=Config::get("$key.$database", $default);                
+        }
+        if (! is_int($t))
+            throw new Exception('Configuration : valeur incorrecte pour la clé ' . $key . ', entier attendu.');
+        return $t;
+    }
+    
+    private function expect(XMLReader $xml, $name)
+    {
+        if ($xml->name !== $name) $xml->next($name);
+        if ($xml->name !== $name)
+            throw new DumpException('<'. $name . '> attendu : ' . $this->nodeType($xml));
+    }
+    
+    private function nodeType($xml)
+    {
+        switch($xml->nodeType)
+        {
+            case XMLReader::NONE: return 'none';
+            case XMLReader::ELEMENT: return 'element';
+            case XMLReader::ATTRIBUTE: return 'attribute';
+            case XMLReader::TEXT: return 'texte';
+            case XMLReader::CDATA: return 'cdata';
+            case XMLReader::ENTITY_REF: return 'entity_ref';
+            case XMLReader::ENTITY: return 'entity';
+            case XMLReader::PI: return 'PI';
+            case XMLReader::COMMENT: return 'comment';
+            case XMLReader::DOC: return 'doc';
+            case XMLReader::DOC_TYPE: return 'doc_type';
+            case XMLReader::DOC_FRAGMENT: return 'doc_fragment';
+            case XMLReader::NOTATION: return 'notation';
+            case XMLReader::WHITESPACE: return 'whitespace';
+            case XMLReader::SIGNIFICANT_WHITESPACE: return 'significant_whitespace';
+            case XMLReader::END_ELEMENT: return 'end_element';
+            case XMLReader::END_ENTITY: return 'end_entity';
+            case XMLReader::XML_DECLARATION: return 'xml_declaration';
+            default : return 'type inconnu';
+        }
+    }
+    
+    private function decode(DOMElement $node)
+    {
+        $value=$node->nodeValue;
+        switch ($node->getAttribute('xsi:type'))
+        {
+            case '': 
+                $value=utf8_decode($value);
+                break;
+                
+            case 'xsd:base64Binary':
+                $value=base64_decode($value);
+                break;
+                
+            default:
+                throw new DumpException('Valeur non supportée pour l\'attribut xsi:type : ' . $node->getAttribute('xsi:type')); 
+        }
+        return $value;
+    }
+    
+    private function fieldValue(DOMElement $node)
+    {
+//        $doc=new DOMDocument();
+//        $doc->appendChild($node);
+//        echo '<textarea cols="120" rows="10">', $doc->saveXML(), '</textarea>'; 
+        
+        if (!$node->hasChildNodes()) 
+        {
+//            echo 'champ sans fils, chaine vide<br />';
+            return '';
+        }
+        
+        if ($node->childNodes->length===1 && $node->firstChild->nodeType===XML_TEXT_NODE)
+        {
+            return $this->decode($node);
+        }
+            
+//        echo 'champ avec ', $node->childNodes->length, ' fils<br />';
+        $value=array();
+        foreach ($node->childNodes as $child)
+        {
+//            echo 'fils : ', var_export($child, true), '<br />';
+            switch($child->nodeType)
+            {
+                case XML_ELEMENT_NODE:
+                    if ($child->nodeName !== 'item')
+                        throw new DumpException('<item> attendu');
+//                    echo 'nouvel item : ', $child->nodeValue, '<br />';
+                    $value[]=$this->decode($child);
+                    break;
+                    
+                case XML_TEXT_NODE:
+                    if (! $child->isWhitespaceInElementContent())
+                        throw new DumpException('seuls des blancs sont autorisés entre des tags <item>'.var_export($child->nodeValue, true));
+//                    echo 'noeud type texte contenant des clancs<br />';
+                    break;
+                    
+                default:
+                    throw new Exception('Type de noeud non autorisé entre des tags <item>' . var_export($child,true));
+            }
+        }
+        return $value;
+    }
+    
+    public function actionRestore($database, $filename=null)
+    {
+        // Ouvre le fichier
+        $xml=new XMLReader();
+        $xml->open(dirname(__FILE__).'/dump.xml');
+        $xml->read();
+        echo 'initial : ', $xml->name,  '<br />';
+        
+        // <database>
+        $this->expect($xml, 'database');
+        $xml->read();
+        
+        // <schema>
+        $this->expect($xml, 'schema');
+            
+        $doc=new DOMDocument();
+        $doc->appendChild($xml->expand());
+        $dbs=new DatabaseSchema($doc->saveXML()); 
+        echo $dbs->validate() ? 'schéma valide' : 'schéma invalide', '<br />';
+        // var_export($dbs->toXml());
+        $xml->next();
+        
+        // <records>
+        $this->expect($xml, 'records');
+        $xml->read();
+        
+        while($xml->name !== 'records' || $xml->nodeType !== XMLReader::END_ELEMENT)
+        {
+            $this->expect($xml, 'row');
+            $xml->read();
+
+            while($xml->name !== 'row' || $xml->nodeType !== XMLReader::END_ELEMENT)
+            {
+                while($xml->nodeType !== XMLReader::ELEMENT) $xml->read();
+                
+                $field=$xml->name;
+                $value=$this->fieldValue($xml->expand());
+                if (is_array($value))
+                    echo "<b>$field</b>", ': ', var_export(implode('¤', $value),true), '<br />';
+                else
+                    echo "<b>$field</b>", ': ', var_export($value,true), '<br />';
+                $xml->next();
+            }
+            echo '<hr />';
+            $xml->read();
+            
+        }
+        $xml->read(); // </records>
+        echo $xml->name, ' : ', $this->nodeType($xml), ' = ', $xml->value, '<br />';
+        echo 'après records: ', $xml->name,  '<br />';
+        
+        
+        $xml->close();
+        $xml=null;
+        die('ok');
+        
+    }
+    
+}
+class DumpException extends Exception
+{
+    public function __construct($message)
+    {
+        parent::__construct('Dump invalide : ' . htmlspecialchars($message));
     }
 }
 ?>
