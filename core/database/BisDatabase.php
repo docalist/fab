a6fb76bb21cb76369f8f88535335cb681013941c
Daniel Menard <daniel.menard@laposte.net>
2009-03-18 14:15:03 +0000

    BisDatabase : ajustement du nom des options - J'ai oublié de préciser dans le dernier commit que désormais   les noms des options ne commençait plus par "_" dans   xapianDatabase (elles ont toujours un underscore lorsqu'elles   sont utilisées en query string, mais pas en interne). - Modification de BisDatabase en conséquence (suppression du   caractère underscore de début partout où les options étaient   utilisées).
    git-svn-id: http://fab.googlecode.com/svn/trunk@1014 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- core/database/BisDatabase.php
+++ core/database/BisDatabase.php
@@ -10,7 +10,7 @@
 
 /**
  * Représente une base de données Bis
- * 
+ *
  * @package     fab
  * @subpackage  database
  */
@@ -20,71 +20,71 @@ class BisDatabase extends Database
      * @var COM.Bis.Selection l'objet COM contenant la sélection en cours
      */
     private $selection=null;
-    
+
     /**
      * @var boolean true : afficher en ordre inverse
      */
     private $reverse=false;
-    
+
     /**
      * @var int le "rang" de la notice en cours
      */
     private $rank=0;
-    
+
     /**
      * @var int le rang de la première notice à retourner
      */
     private $start=0;
-    
+
     /**
      * @var int le nombre maximum de notices à retourner
      */
     private $max=0;
-    
+
     private $maxRank=PHP_INT_MAX;
-    
+
     protected function doCreate($database, $def, $options=null)
     {
         throw new Exception('non implémenté');
     }
-        
+
     protected function doOpen($database, $readOnly=true)
     {
         // Ouvre la base de données
         $bis=new COM('Bis.Engine');
         $db=$bis->openDatabase($database, false, $readOnly);
-        
+
         // Détermine le nom du premier (et normallement unique) dataset
         $dataset=$db->datasets(1)->name;
-        
+
         // Ouvre une sélection sur ce dataset
         $this->selection=$db->openSelection($dataset);
 
         // Crée l'objet record
         $this->record=new BisDatabaseRecord($this, $this->selection->fields);
     }
-        
+
     public function search($equation=null, $options=null)
     {
-        
+
         // a priori, pas de réponses
         $this->eof=true;
 
-        // Analyse les options indiquées (start et sort) 
+        // Analyse les options indiquées (start et sort)
         if (is_array($options))
         {
-            $sort=isset($options['_sort']) ? $options['_sort'] : null;
+            $sort=isset($options['sort']) ? $options['sort'] : null;
 //            if (is_array($sort))
 //                foreach ($sort as $i)
 //                    if ($i) { $sort=$i; break;}
 
-            $start=isset($options['_start']) ? ((int)$options['_start'])-1 : 0;
+            $start=isset($options['start']) ? ((int)$options['start'])-1 : 0;
 //            if (is_array($start))
 //                foreach ($start as $i)
 //                    if ($i) { $start=$i; break;}
             if ($start<0) $start=0;
 
-            $max=isset($options['_max']) ? $options['_max'] : 10;
+            $max=isset($options['max']) ? $options['max'] : 10;
             if (is_array($max))
                 foreach ($max as $i)
                     if ($i) { $max=$i; break;}
@@ -105,7 +105,7 @@ class BisDatabase extends Database
         $this->start=$start+1;
         $this->max=$max;
 //        echo 'equation=', $equation, ', options=', print_r($options,true), ', sort=', $sort, ', start=', $start, "\n";
-        
+
         // Lance la recherche
         $this->rank=0;
         $this->selection->equation=$equation;
@@ -118,28 +118,28 @@ class BisDatabase extends Database
         {
             $this->selection->moveLast();
             $this->selection->moveNext();
-            return false;	
+            return false;
         }
 
         $this->rank=$this->start;
-        
+
         $this->maxRank=($this->max==-1 ? PHP_INT_MAX : $this->start+$this->max-1);
 
         // Gère l'ordre de tri et va sur la start-ième réponse
         switch($sort)
         {
         	case '%':
-            case '-': 
+            case '-':
                 $this->reverse=true;
-                $this->selection->moveLast(); 
+                $this->selection->moveLast();
                 while ($start--) $this->selection->movePrevious();
                 break;
-                
+
             default:
                 $this->reverse=false;
                 while ($start--) $this->selection->moveNext();
         }
-        
+
         // Retourne le résultat
         return ! $this->eof=($this->max === 0);
     }
@@ -150,24 +150,26 @@ class BisDatabase extends Database
     }
 
     public function searchInfo($what)
-    {        
+    {
     	switch ($what)
         {
         	case 'equation': return $this->selection->equation;
             case 'rank': return $this->rank;
-            case '_start': return $this->start;
+            case '_start':
+            case 'start':
+                return $this->start;
             case 'max': return $this->max;
             default: return null;
         }
     }
-    
+
     public function moveNext()
     {
         if ( $this->rank >= $this->maxRank )
             return !$this->eof=true;
-            
+
         $this->rank++;
-        if ($this->reverse) 
+        if ($this->reverse)
         {
             $this->selection->movePrevious();
             return !$this->eof=$this->selection->bof;
@@ -215,7 +217,7 @@ class BisDatabase extends Database
 
 /**
  * Représente un enregistrement dans une base {@link BisDatabase}
- * 
+ *
  * @package     fab
  * @subpackage  database
  */
@@ -225,22 +227,22 @@ class BisDatabaseRecord extends DatabaseRecord
      * @var COM.Bis.Fields Liste des champs de cet enregistrement
      */
     private $fields=null;
-    
+
     /**
      * @var int Numéro du champ en corus (utilisé pour Iterator)
      */
     private $current=0;
-    
+
     /**
      * {@inheritdoc}
-     * 
+     *
      * @param COM.Bis.Fields $fields l'objet BIS.Fields contenant la liste
      * des champs de la base
      */
     public function __construct(Database $parent, & $fields)
     {
         parent::__construct($parent);
-        $this->fields= & $fields;   
+        $this->fields= & $fields;
     }
 
     /* <ArrayAccess> */
@@ -255,10 +257,10 @@ class BisDatabaseRecord extends DatabaseRecord
 //        $h=$this->fields[$offset]->value;
 //        if (strpos($h, '¨') == false) return $h;
 //        return explode('¨', $h);
-        
+
 //        if (isChampArticle($offset))
 //            $h=explode($h, SepUsedInDatabase());
-//            
+//
         return $this->fields[$offset]->value;
         //return (string) $this->fields[$offset]->value;
         //return $this->fields->item($offset)->value;
@@ -275,20 +277,20 @@ class BisDatabaseRecord extends DatabaseRecord
     }
 
     /* </ArrayAccess> */
-    
+
 
     /* <Countable> */
 
     public function count()
     {
-        return $this->fields->count;    
+        return $this->fields->count;
     }
 
     /* </Countable> */
 
-    
+
     /* <Iterator> */
-    
+
     public function rewind()
     {
         $this->current=1;
@@ -317,9 +319,9 @@ class BisDatabaseRecord extends DatabaseRecord
     {
         return $this->current<=$this->fields->count;
     }
-    
+
     /* </Iterator> */
-    
+
 }
 
 ?>
