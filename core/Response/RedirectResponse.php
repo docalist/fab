704e3d1627046d179e6c02c16bb2ba8ad7f6608b
Daniel Menard <daniel.menard@laposte.net>
2010-04-02 11:11:07 +0000

    RedirectResponse : nouvelle classe représentant une redirection http.
    La réponse prend en paramètre l'url ou la fab url vers laquelle l'utilisateur doit être redirigé.
    
    La redirection peut être temporaire (code 302) ou permanente (301).
    
    La réponse générée contient un bref message html et des instructions de redirection pour http (Location), pour html (meta refresh) et pour javascript (window.location).
    
    
    git-svn-id: http://fab.googlecode.com/svn/trunk@1125 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

---
+++ core/Response/RedirectResponse.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * @package     fab
+ * @subpackage  response
+ * @author      Daniel Ménard <Daniel.Menard@ehesp.fr>
+ * @version     SVN: $Id$
+ */
+
+/**
+ * Représente une redirection temporaire ou permanente.
+ *
+ * @package     fab
+ * @subpackage  response
+ */
+class RedirectResponse extends Response
+{
+    /**
+     * @inheritdoc
+     */
+    protected $contentType = 'text/html';
+
+
+    /**
+     * @inheritdoc
+     */
+    protected $charset = 'ISO-8859-1';
+
+
+    /**
+     * @inheritdoc
+     */
+    protected $status = 302; // 'Moved Temporarily' en HTTP 1.0
+
+
+    /**
+     * Url vers laquelle cette réponse redirgera l'utilisateur.
+     *
+     * @var string
+     */
+    protected $location = null;
+
+
+    /**
+     * Construit la redirection.
+     *
+     * @param string|null $location l'adresse vers laquelle l'utilisateur sera redirigée. Il peut
+     * s'agit d'une url relative ou absolue ou encore d'une fab-url. Si vous n'indiquez pas
+     * d'adresse, le contenu de la vairable d'environnement HTTP_REFERER est utilisée. Si celle-ci
+     * est vide, une exception est générée.
+     *
+     * @param bool $permanent par défaut la requête génére une redirection temporaire (302). Si vous
+     * indiquez true pour ce paramètre, une redirection permanente (301) sera générée à la place.
+     *
+     * @throw Exception si aucune url n'a été indiquée et que HTTP_REFERER est vide.
+     */
+    public function __construct($location = null, $permanent = false)
+    {
+        // Laisse les ancêtres faire leur boulot
+        parent::__construct();
+
+        // Si aucune url n'a été indiquée, utilise le referrer
+        if (! $location)
+        {
+            if (isset($_SERVER['HTTP_REFERER']))
+                $location = $_SERVER['HTTP_REFERER'];
+            else
+                throw new Exception('Impossible de déterminer l\'url de redirection');
+        }
+
+        // Route l'adresse en url absolue
+        if (preg_match('~^[a-zA-Z]{3,6}:~', $location) === 0)
+            $location = Utils::getHost() . $location;
+        else
+            $location = Routing::linkFor($location, true);
+
+        // Change le statut par défaut si c'est une redirection permanente
+        if ($permanent)
+            $this->status = 301;
+
+        // Ajoute l'entête de redirection
+        $this->setHeader('Location', $location);
+
+        // Définit le template qui sera utilisé pour générer la réponse
+        parent::template
+        (
+            $this,
+            dirname(__FILE__) . '/RedirectResponse.html',
+            array
+            (
+                'location'=>$location,
+                'status' => $this->status,
+                'reason' => self::$httpStatus[$this->status]
+            )
+        );
+    }
+
+
+    /**
+     * Génère une exception si vous essayez de définir un contenu pour une requête de ce type.
+     *
+     * @param mixed $content ignoré
+     *
+     * @throw Exception systématiquement.
+     */
+    public function setContent($content = null)
+    {
+        throw new Exception('Impossible de définir un contenu pour une réponse de type '. get_class($this));
+    }
+
+
+    /**
+     * Génère une exception si vous essayez de définir un contenu pour une requête de ce type.
+     *
+     * @param mixed $content ignoré
+     *
+     * @throw Exception systématiquement.
+     */
+    public function prependContent($content)
+    {
+        throw new Exception('Illegal pour une réponse de type ' . get_class($this));
+    }
+
+
+    /**
+     * Génère une exception si vous essayez de définir un contenu pour une requête de ce type.
+     *
+     * @param mixed $content ignoré
+     *
+     * @throw Exception systématiquement.
+     */
+    public function appendContent($content)
+    {
+        throw new Exception('Illegal pour une réponse de type ' . get_class($this));
+    }
+
+    /**
+     * Génère une exception si vous essayez de définir un contenu pour une requête de ce type
+     *
+     * @param object $context ignoré
+     * @param string $path ignoré
+     * @param mixed $data ... ignoré
+     *
+     * @throw Exception systématiquement.
+     */
+    public function setTemplate($context, $path, $data=null)
+    {
+        $this->content(''); // évite de dupliquer le code de l'exception
+    }
+}
