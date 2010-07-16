c750d18734dd94df63677ac06ba55998a871d92e
Daniel Menard <daniel.menard@laposte.net>
2010-07-16 16:21:13 +0000

    AdminSchemas : saveSchema(), retourne un objet JSonResponse() pour que l'éditeur de schéma fonctionne à nouveau.
    git-svn-id: http://fab.googlecode.com/svn/trunk@1196 f0e09bc5-ef39-0410-9dc8-597abd2d75a8

--- modules/AdminSchemas/AdminSchemas.php
+++ modules/AdminSchemas/AdminSchemas.php
@@ -9,44 +9,44 @@
 
 /**
  * Module d'administration des schémas.
- * 
+ *
  * @package     fab
  * @subpackage  Admin
  */
 /**
- * Module d'administration permettant de gérer les {@link DatabaseSchema schémas 
- * de bases de données de l'application}. 
- * 
- * Les schémas de bases de données sont des fichiers 
- * {@link http://fr.wikipedia.org/wiki/Xml xml} stockés dans le répertoire 
+ * Module d'administration permettant de gérer les {@link DatabaseSchema schémas
+ * de bases de données de l'application}.
+ *
+ * Les schémas de bases de données sont des fichiers
+ * {@link http://fr.wikipedia.org/wiki/Xml xml} stockés dans le répertoire
  * <code>/data/schemas</code> de l'application et qui définissent la structure
  * d'une base de données (liste des champs, des index, paramètres, etc.)
- * 
- * Ce module permet de lister tous les schémas de l'application ainsi que les 
+ *
+ * Ce module permet de lister tous les schémas de l'application ainsi que les
  * {@link /AdminSchemas modèles de schémas proposés par fab}.
- * 
- * Il hérite du module {@link AdminFiles} et dispose donc des actions 
- * de base permettant de {@link AdminFiles::actionCopy() copier}, 
- * {@link AdminFiles::actionRename() renommer}, 
- * {@link AdminFiles::actionDelete() supprimer} et 
+ *
+ * Il hérite du module {@link AdminFiles} et dispose donc des actions
+ * de base permettant de {@link AdminFiles::actionCopy() copier},
+ * {@link AdminFiles::actionRename() renommer},
+ * {@link AdminFiles::actionDelete() supprimer} et
  * {@link AdminFiles::actionDownload() télécharger} un fichier de schéma ainsi
  * que de la possibilité {@link actionEdit() d'éditer le code xml} d'un schéma
- * ou de {@link AdminFiles::copyFrom() créer un nouveau schéma à partir d'un 
+ * ou de {@link AdminFiles::copyFrom() créer un nouveau schéma à partir d'un
  * modèle proposé par fab}.
- * 
+ *
  * <code>AdminSchemas</code> introduit un éditeur spécifique qui permet de
- * {@link actionNew() créer}, {@link actionEditSchema() modifier} et 
+ * {@link actionNew() créer}, {@link actionEditSchema() modifier} et
  * {@link actionSaveSchema() sauvegarder} un schéma de façon plus intuitive
  * qu'en intervenant sur le code xml sous-jacent et dispose d'une fonction de
- * validation intégrée qui empêche la création d'un 
+ * validation intégrée qui empêche la création d'un
  * {@link DatabaseSchema::validate() schéma invalide}.
- * 
+ *
  * Pour plus d'informations, vous pouvez consulter :
- * - La documentation de l'action {@link AdminDatabases::actionSetSchema SetSchema} 
- *   du module d'administration {@link /AdminDatabases AdminDatabases} qui permet 
+ * - La documentation de l'action {@link AdminDatabases::actionSetSchema SetSchema}
+ *   du module d'administration {@link /AdminDatabases AdminDatabases} qui permet
  *   d'appliquer un schéma à une base de données.
  * - {@link DatabaseSchema l'API de la classe DatabaseSchema}.
- * 
+ *
  * @package     fab
  * @subpackage  Admin
  */
@@ -54,7 +54,7 @@ class AdminSchemas extends AdminFiles
 {
     /**
      * Page d'accueil du module d'administration des schémas de bases de données.
-     * 
+     *
      * Affiche la liste des schémas disponibles.
      */
     public function actionIndex()
@@ -64,25 +64,25 @@ class AdminSchemas extends AdminFiles
             Config::get('template')
         );
     }
-    
-        
+
+
     /**
      * Retourne la liste des schémas connus du système.
      *
      * Par défaut, la fonction retourne la liste des schémas de l'application.
      * Si $fab est à true, c'est la liste des schémas de fab qui sera retournée.
-     * 
+     *
      * @param bool $fab true pour récupérer les schémas définis dans fab,
      * false (valeur par défaut) pour retourner les schémas de l'application.
-     * 
-     * @return array un tableau contenant le path de tous les schémas 
+     *
+     * @return array un tableau contenant le path de tous les schémas
      * disponibles (la clé associé contient le nom du schéma, c'est-à-dire
      * <code>basename($path)</code>).
      */
     public static function getSchemas($fab=false)
     {
         $path=($fab ? Runtime::$fabRoot : Runtime::$root) . 'data/schemas/';
-        
+
         // Construit la liste
         $files=glob($path.'*.xml');
         if ($files===false) return array();
@@ -92,17 +92,17 @@ class AdminSchemas extends AdminFiles
         {
             $schemas[$file]=basename($file);
         }
-        
+
         // Trie par ordre alphabétique du nom
         uksort($schemas, 'strcoll');
 
         return $schemas;
     }
-    
-    
+
+
     /**
      * Retourne le schéma dont le nom est passé en paramètre.
-     *  
+     *
      * @return DatabaseSchema|false
      */
     public static function getSchema($schema)
@@ -113,10 +113,10 @@ class AdminSchemas extends AdminFiles
         return new DatabaseSchema(file_get_contents($path));
     }
 
-    
+
     /**
      * Edite un schéma de l'application.
-     * 
+     *
      * @param string $file le nom du fichier xml à éditer
      */
     public function actionEditSchema($file)
@@ -127,10 +127,10 @@ class AdminSchemas extends AdminFiles
         $path=Runtime::$root.$dir.$file;
         if (! file_exists($path))
             throw new Exception("Le schéma $file n'existe pas.");
-            
+
         // Charge le schéma
         $schema=new DatabaseSchema(file_get_contents($path));
-                
+
         // Valide et redresse le schéma, ignore les éventuelles erreurs
         $schema->validate();
 
@@ -147,31 +147,31 @@ class AdminSchemas extends AdminFiles
             )
         );
     }
-    
-    
+
+
     /**
      * Vérifie et sauvegarde un schéma.
-     * 
-     * Cette action permet d'enregistrer un schéma modifié avec l'éditeur de 
+     *
+     * Cette action permet d'enregistrer un schéma modifié avec l'éditeur de
      * structure.
-     * 
-     * Elle commence par valider le schéma passé en paramètre. Si des 
+     *
+     * Elle commence par valider le schéma passé en paramètre. Si des
      * erreurs sont détectées, une réponse au format JSON est générée. Cette
      * réponse contient un tableau contenant la liste des erreurs rencontrées.
      * La réponse sera interprétée par l'éditeur de schéma qui affiche la
      * liste des erreurs à l'utilisateur.
-     * 
+     *
      * Si aucune erreur n'a été détectée, le schéma est enregistré.
-     * Dans ce cas, une chaine de caractères au format JSON est retournée 
-     * à l'éditeur. Elle indique l'url vers laquelle l'utilisateur va être 
-     * redirigé. 
+     * Dans ce cas, une chaine de caractères au format JSON est retournée
+     * à l'éditeur. Elle indique l'url vers laquelle l'utilisateur va être
+     * redirigé.
      *
      * @param string $file le nom du fichier xml dans lequel enregistrer le
      * schéma.
-     *   
+     *
      * @param string $schema une chaine de caractères au format JSON contenant le
      * schéma à valider et à enregistrer.
-     * 
+     *
      * @throws Exception si le fichier indiqué n'existe pas.
      */
     public function actionSaveSchema($file, $schema)
@@ -182,39 +182,37 @@ class AdminSchemas extends AdminFiles
         $path=Runtime::$root.$dir.$file;
         if (! file_exists($path))
             throw new Exception("Le schéma $file n'existe pas.");
-        
+
         // Charge le schéma
         $schema=new DatabaseSchema($schema);
-        
+
         // Valide le schéma et détecte les erreurs éventuelles
         $result=$schema->validate();
-        
+
         // S'il y a des erreurs, retourne un tableau JSON contenant la liste
         if ($result !== true)
-        {
-            header('Content-type: application/json; charset=iso-8859-1');
-            echo json_encode(Utils::utf8Encode($result));
-            return;
-        }
-        
+            return Response::create('JSON')->setContent($result);
+
         // Compile le schéma (attribution des ID, etc.)
         $schema->compile();
-        
+
         // Met à jour la date de dernière modification (et de création éventuellement)
         $schema->setLastUpdate();
-        
+
         // Aucune erreur : sauvegarde le schéma
         file_put_contents($path, $schema->toXml());
-        
+
         // Retourne l'url vers laquelle on redirige l'utilisateur
-        header('Content-type: application/json; charset=iso-8859-1');
-        echo json_encode(Routing::linkFor($this->request->clear()->setAction('index'), true));
+        return Response::create('JSON')->setContent
+        (
+            Routing::linkFor($this->request->clear()->setAction('index'), true)
+        );
     }
-    
-    
+
+
     /**
      * Crée un nouveau schéma (vide).
-     * 
+     *
      * Vérifie que le fichier indiqué existe, demande le nouveau nom
      * du fichier, vérifie que ce nom n'est pas déjà pris, renomme le
      * fichier, redirige vers la page d'accueil.
@@ -230,13 +228,13 @@ class AdminSchemas extends AdminFiles
             $file=Utils::defaultExtension($file, '.xml');
             if (Utils::getExtension($file) !== '.xml')
                 $file.='.xml';
-        
+
             // Vérifie que le fichier indiqué n'existe pas déjà
             $path=Runtime::$root.$dir.$file;
             if ($file !== '' && file_exists($path))
                 $error='Il existe déjà un fichier portant ce nom.';
         }
-                        
+
         if ($file==='' || $error !='')
         {
             Template::run
@@ -246,19 +244,19 @@ class AdminSchemas extends AdminFiles
             );
             return;
         }
-        
+
         // Crée un nouveau schéma
         $schema=new DatabaseSchema();
-        
+
         // Enregistre le schéma dans le fichier indiqué
         file_put_contents($path, $schema->toXml());
-                
+
         Runtime::redirect('/'.$this->module);
     }
-    
+
     /**
      * Slot permettant à un autre module de choisir un schéma.
-     * 
+     *
      * @param string $link le lien à appliquer à chaque schéma.
      * @param bool $fab true pou afficher les schémas de l'application,
      * false pour afficher ceux de fab.
